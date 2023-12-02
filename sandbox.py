53b866d212463e028468c0788d97eac501a76b65
Jeremy Allenspach <jeremy.allenspach@ost.ch>
2023-12-02 16:42:31 +0100

    Smith Chart now has all Impedance and Admittance Circles

--- sandbox.py
+++ sandbox.py
@@ -20,8 +20,8 @@ class SmithChart(object):
         self.fig = plt.figure(figsize=(10, 10))
         self.ax = self.fig.add_subplot()
         self.ax.axis('off')
-        self.ax.set_ylim(-0.25, 1.25)
-        self.ax.set_xlim(-0.25, 1.25)
+        self.ax.set_ylim(-1.25, 1.25)
+        self.ax.set_xlim(-1.25, 1.25)
         self.draw_smith_chart()
 
     def show(self):
@@ -37,33 +37,55 @@ class SmithChart(object):
 
     def draw_chart(self):
         # Draw Outline Circle
-        self.smith_circle = Circle((0.5, 0.5), 0.5, fc='none', ec='black')
+        self.smith_circle = Circle((0, 0), 1, fc='none', ec='black')
         self.ax.add_patch(self.smith_circle)
 
         # Draw Horizontal Line
-        line = FancyArrowPatch((0, 0.5), (1, 0.5))
+        line = FancyArrowPatch((-1, 0), (1, 0))
         self.ax.add_patch(line)
 
     def draw_impedance_circle(self):
         for constant_real in self.normalized_values:
-            intersect = constant_real / (constant_real + 1)
-            radius = (1-intersect) / 2
-            center = intersect + radius
-            circle = Circle((center, 0.5), radius, fc='none', ec='blue')
+
+            # Draw Constant Real Impedance Circles
+            center = (constant_real / (constant_real + 1), 0)
+            radius = 1 / (constant_real + 1)
+            circle = Circle(center, radius, fc='none', ec='blue')
             self.ax.add_patch(circle)
+
+            # Constant imaginary Impedance Circles
+            i_center = (1, 1/constant_real)
+            i_center_n = (1, -1/constant_real)
+            i_radius = 1/constant_real
+            circle = Circle(i_center, i_radius, fc='none', ec='red')
+            neg_const = Circle(i_center_n, i_radius, fc='none', ec='red')
+            self.ax.add_patch(circle)
+            self.ax.add_patch(neg_const)
 
     def draw_admittance_circle(self):
         for constant_real in self.normalized_values:
-            intersect = 1 - (constant_real / (constant_real + 1))
-            radius = intersect / 2
-            center = intersect - radius
-            circle = Circle((center, 0.5), radius, fc='none', ec='red')
+
+            # Draw Constant Real Admittance Circles
+            center = (- (constant_real/ (constant_real + 1)), 0)
+            print(center)
+            radius = 1 / (constant_real + 1)
+            circle = Circle(center, radius, fc='none', ec='red')
             self.ax.add_patch(circle)
+
+            # Draw Constant Imaginary Admittance Circles
+            # Constant imaginary Impedance Circles
+            i_center = (-1, 1 / constant_real)
+            i_center_n = (-1, -1 / constant_real)
+            i_radius = 1 / constant_real
+            circle = Circle(i_center, i_radius, fc='none', ec='red')
+            neg_const = Circle(i_center_n, i_radius, fc='none', ec='red')
+            self.ax.add_patch(circle)
+            self.ax.add_patch(neg_const)
 
     def add_text(self):
         # Add Normalizing Impedance Text Box
         z0_text = f"$Z_0:$ {self.z0}"
-        z0_box = Rectangle(xy=(0.85, 0.9), width=0.2, height=0.1, ec='black', fc='none')
+        z0_box = Rectangle(xy=(2, 2), width=0.2, height=0.1, ec='black', fc='none')
         self.ax.add_patch(z0_box)
         rx, ry = z0_box.get_xy()
         tx = rx + z0_box.get_width() / 2.0
@@ -73,12 +95,12 @@ class SmithChart(object):
         # Add Start Impedance
         start_impedance = complex(120)
         start = f"$Z_{{start}}:${start_impedance}Ω"
-        self.ax.annotate(start, (0, 0), color='red', fontsize=self.text_size)
+        self.ax.annotate(start, (-1, -1), color='red', fontsize=self.text_size)
 
         # Add Target Impedance Text
         target_impedance = complex(60, 0)
         target = f"$Z_{{target}}:${target_impedance}Ω"
-        self.ax.annotate(target, (0.75, 0), color='green', fontsize=self.text_size)
+        self.ax.annotate(target, (1, -1), color='green', fontsize=self.text_size)
 
         # Add Component Values
 
