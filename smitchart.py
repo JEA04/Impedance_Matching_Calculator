08a3d11925fdf191a1e0cbd68b6c41b78d831025
Jeremy Allenspach <jeremy.allenspach@ost.ch>
2023-12-03 17:58:54 +0100

    Fixed real comparison problem

--- smitchart.py
+++ smitchart.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 import numpy
 import numpy as np
 from matplotlib.patches import Circle, FancyArrowPatch, Rectangle
-from matplotlib.lines import Line2D
 
 
 class SmithChart(object):
@@ -27,6 +26,46 @@ class SmithChart(object):
         self.ax.axis('off')
         self.ax.axis(np.array([-1.1, 1.1, -1.1, 1.1]))
         self.draw_smith_chart()
+
+    def plot(self, *args, **kwargs):
+        new_points = []
+        for arg in args:
+            new_points.append(arg)
+
+        previous_point = None
+        for index, point in enumerate(new_points):
+            xy = self.impedance_to_gamma(point)
+            if index == 0:
+                self.ax.plot(xy.real, xy.imag, 'x', color='red')
+                self.add_start_impedance_text(args[index])
+            elif index == len(new_points) - 1:
+                self.ax.plot(xy.real, xy.imag, 'o', color='green')
+                self.add_target_impedance_text(args[index])
+                self.draw_curve_between_points(previous_point, point)
+            else:
+                # TODO: Interpolate between points
+                self.draw_curve_between_points(previous_point, point)
+            previous_point = point
+
+    def draw_curve_between_points(self, previous, current):
+        inter_circle_x = []
+        inter_circle_y = []
+        if previous.imag > current.imag and round(previous.real,3) != round(current.real, 3):
+            inter_points = np.linspace((1/previous).imag, (1/current).imag, num=50)
+            for point in inter_points:
+                admittance_point = complex((1/previous).real, point)
+                xy = self.admittance_to_gamma(admittance_point)
+                inter_circle_x.append(xy.real)
+                inter_circle_y.append(xy.imag)
+        else:
+            # Work with Impedance
+            inter_points = np.linspace(previous.imag, current.imag, num=50)
+            for point in inter_points:
+                xy = self.impedance_to_gamma(complex(previous.real, point))
+                inter_circle_x.append(xy.real)
+                inter_circle_y.append(xy.imag)
+        self.ax.plot(inter_circle_x, inter_circle_y, '-', color='gray', zorder=1)
+        return
 
     def draw_smith_chart(self):
         self.draw_chart()
@@ -96,7 +135,7 @@ class SmithChart(object):
 
     def add_start_impedance_text(self, value):
         start = f"$Z_{{start}}:${value}Ω"
-        self.z_start_text =  self.ax.annotate(start, (-1.0, -1.1), color='red',
+        self.z_start_text = self.ax.annotate(start, (-1.0, -1.1), color='red',
                                               fontsize=self.text_size, ha='left', va='center')
 
     def add_target_impedance_text(self, value):
@@ -104,35 +143,17 @@ class SmithChart(object):
         self.z_target_text = self.ax.annotate(target, (1, -1.1), color='green',
                                               fontsize=self.text_size, ha='right', va='center')
 
-    def add_component_values(self, component_1, component_2):
-        components_text = f"[1] {component_1} \n[2] {component_2}"
-        self.ax.annotate(components_text, (-1.0, 1.0), color='black',
+    def add_component_values(self, c1, c2):
+        components_text = f"[1] {c1[0]}: {c1[1]} {c1[2]}\n[2] {c2[0]}: {c2[1]} {c2[2]}"
+        self.ax.annotate(components_text, (-1.1, 1.0), color='black',
                          fontsize=self.text_size, ha='left', va='center')
-
-    def plot(self, *args, **kwargs):
-        new_args = []
-        for arg in args:
-            xy = self.impedance_to_gamma(arg)
-            new_args.append([xy.real, xy.imag])
-
-        for index, [real, imag] in enumerate(new_args):
-            if index == 0:
-                self.ax.plot(real, imag, 'x', color='red')
-                self.add_start_impedance_text(args[index])
-            elif index == len(new_args) - 1:
-                self.ax.plot(real, imag, 'o', color='green')
-                self.add_target_impedance_text(args[index])
-            else:
-                # TODO: Interpolate between points
-                print("Else")
-
-        #self.ax.plot(gamma.real, gamma.imag, 'o', color="green")
 
     def impedance_to_gamma(self, impedance):
         return complex(impedance - self.z0) / (impedance + self.z0)
 
     def admittance_to_gamma(self, admittance):
-        return complex(admittance - 1 / self.z0) / (admittance + 1 / self.z0)
+        y0 = 1/self.z0
+        return complex(-(admittance - y0)/(admittance + y0))
 
     def set_z0(self, value):
         if value > 0:
@@ -141,13 +162,21 @@ class SmithChart(object):
 
 
 if __name__ == '__main__':
-    fig = plt.figure(figsize=(12, 12))
-    for i in range(1, 4):
-        ax = fig.add_subplot(2, 2, i)
-        z_start = complex(100, 75)
-        z_end = complex(60, 80)
-        sc = SmithChart(ax)
-        sc.set_z0(60)
-        sc.plot(z_start, z_end)
-        sc.add_component_values('Lp: 7.83nH', 'Cs: 1.09 pF')
+    fig = plt.figure(figsize=(10, 10))
+    # for i in range(1, 4):
+    #     ax = fig.add_subplot(2, 2, i)
+    #     z_start = complex(100, 75)
+    #     z_end = complex(60, 80)
+    #     sc = SmithChart(ax)
+    #     sc.set_z0(60)
+    #     sc.plot(z_start, complex(120, 0), z_end)
+    #     sc.add_component_values('Lp: 7.83nH', 'Cs: 1.09 pF')
+    ax = fig.add_subplot()
+    z_start = complex(100, 75)
+    middle = complex(30, 61.5)
+    z_end = complex(30, 0)
+    sc = SmithChart(ax)
+    sc.set_z0(50)
+    sc.plot(z_start, middle, z_end)
     fig.show()
+
